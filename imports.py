6eabcdd01a56e5474293b5992baafd46ff116ff3
Nelson Rebelo <rebelo@chalmers.se>
2023-07-19 14:13:21 +0200

    Uniformity of upload data

--- imports.py
+++ imports.py
@@ -6,16 +6,48 @@ import time
 
 """
 Created on July 4th 13:46:57 2023
-This code will be used to plot maps of the 113
-@author: rebelo
+This code will be used to plot maps of the ThetaMetric White Light Reflectance tool
+@author: Nelson Rebelo  @ChalmersUniversity
 """
+
 def find_in_array(array, value):
+    """
+    It will find the index of the matrice element with the closest value given as input
+
+    Parameters
+    ----------
+    array : array of float
+        Data array
+    value : float
+        calue to which you wish to find the index
+
+    Returns
+    -------
+    idx : TYPE
+        DESCRIPTION.
+
+    """
     array = np.asarray(array)
     idx = (np.abs(array - value)).argmin()
     return idx
 
 def get_files(dir_path):
-    # folder path
+    """
+    For a given directoly will list the files into an array and the number of value
+
+    Parameters
+    ----------
+    dir_path : String
+        Directory path. Folder where all the data must be.
+
+    Returns
+    -------
+    count : int.
+    file_paths : List
+        List with the directories path to the files inside the inout directory
+
+    """
+    # folder paths
     file_paths = []
     count = 0
     # Iterate directory
@@ -27,7 +59,31 @@ def get_files(dir_path):
             file_paths.append(filepath) 
     return count,file_paths
 
-def UploadCSVData(file_directory,data_points,min_wav,max_wav):
+def UploadCSVData(file_directory,num_data_points,min_wav,max_wav):
+    """
+    It will uplad all the data from the tool.
+    
+    Each spectrofram and the coordinates of each point must be put inside the same folder.
+
+    Parameters
+    ----------
+    file_directory : string
+        Folder path were all the data is located.
+    min_wav : int
+        Minimum wavelength you want to include.
+    max_wav : int
+        Maximum wavelength you want to include.
+
+    Returns
+    -------
+    data : array
+        reflectance spectrograms for each data point
+    coordinates : array
+        Coordinates of each data point
+    wavelengths : list
+        List with data points wavelength
+
+    """
     count,file_paths = get_files(file_directory)
     
     wavelengths = np.loadtxt(file_directory+"\\0.csv",delimiter=",",skiprows=1,usecols=0)
@@ -35,13 +91,11 @@ def UploadCSVData(file_directory,data_points,min_wav,max_wav):
     min_indx=find_in_array(wavelengths, min_wav)
     max_indx=find_in_array(wavelengths, max_wav)
     
-    data = np.empty((max_indx,data_points))
-    
-    min_indx=find_in_array(wavelengths, min_wav)
-    max_indx=find_in_array(wavelengths, max_wav)
+    data = np.empty((max_indx,num_data_points))
     
     wavelengths=wavelengths[min_indx:max_indx:1]
     
+    #Loop goes through the files to find the coordinates file, read, and exclude it
     for i in range(count):
         filename=file_paths[i]
         if "coordinates" in filename:
@@ -50,21 +104,22 @@ def UploadCSVData(file_directory,data_points,min_wav,max_wav):
             count=count-1
             i=0
             break
-    
+    #Loop goes trhough the files in the folder to take data from each CVS and put it into one array
     for i in range(count):
         filename=file_paths[i]
         tag=int(filename[-10:].translate({ord(i): '' for i in 'Points.csv\\'}))
         data[:,tag] = np.loadtxt(filename,delimiter=",",skiprows=min_indx,usecols=(1),max_rows=max_indx)
-        
+    
+    #Loop that removes data points with a too low R^2, 0.6    
     index=coordinates.shape[0]
     remove=[]
     for c in range(index):
         if coordinates[c,2]<0.6:
             remove.append(c)
-    
     data=np.delete(data,remove,axis=1)
     coordinates=np.delete(coordinates,remove,axis=0)
     
+    #Recenter de coordinates so that the center of the waffer is at (0,0)
     coordinates[:,0]=coordinates[:,0]-50
     coordinates[:,1]=coordinates[:,1]-50
             
@@ -72,37 +127,47 @@ def UploadCSVData(file_directory,data_points,min_wav,max_wav):
 
 
 def FindCenter(data,wavelengths,smoothness):
+    """
     
+
+    Parameters
+    ----------
+    smoothness : Int
+        Size of the kernel for the rolling average (it should not be more than 250)
+    data : array
+        Should come from UploadCSVData() 
+    wavelengths : array
+        Should come from UploadCSVData() 
+
+    Returns
+    -------
+    wavelength_center : array
+        Center of the stopband for each location
+
+    """
+    #Initiate array for data
     index=data.shape[1]
-    index2=data.shape[0]
-    
     wavelength_center=np.empty(index)
     
+    #Parameters for rolling average
     kernel_size = smoothness
     kernel = np.ones(kernel_size) / kernel_size
     
     for i in range(index):
+        #smooth data with a rolling average
         smooth_data = np.convolve(data[:,i], kernel, mode='same')
+        #calculate the derivative of the raw data
         gradiant_sooth_data=np.gradient(smooth_data)
-        gradiant_sooth2_raw = np.convolve(gradiant_sooth_data, kernel, mode='same')
+        #smooth the derivative with a rolling average
+        gradiant_sooth = np.convolve(gradiant_sooth_data, kernel, mode='same')
         
-        temp=gradiant_sooth2_raw
-        maxVal=temp.argmax()
-        minVal=temp.argmin()
-        
-        #Method 1
-        #temp=temp[minVal:index2:1]
-        #minVal=temp.argmax()
-        
-        #Method 2
-        center=round(maxVal+((minVal-maxVal)/2))
-        
-        #Method3
-        #temp=temp[maxVal:minVal:1]
-        #center=find_in_array(temp, 0)
-        
-        wavelength=wavelengths[center]
-        
+        #Calculate the stopband center by calculating the middle point between the absolute maximum and absolute minimu of the derivative
+        maxVal=gradiant_sooth.argmax()
+        minVal=gradiant_sooth.argmin()
+        center=round(maxVal+((minVal-maxVal)/2))#index
+        wavelength=wavelengths[center]#What wavelength does this inx corresponde to
+
+        #Save the result into an array
         wavelength_center[i]=wavelength
         
     return wavelength_center
@@ -166,11 +231,6 @@ def plot_maps(coordinates,wavelength_center,detail,title):
     y=coordinates[:,1]
     z=wavelength_center
     
-    if detail==1:
-        detail_val=30
-    else:
-        detail_val=10
-        
     
     xi = np.linspace(x.min(), x.max(), 1000)
     yi = np.linspace(y.min(), y.max(), 1000)
@@ -185,8 +245,7 @@ def plot_maps(coordinates,wavelength_center,detail,title):
     zi[(zi<zmin) | (zi>zmax)] = None
 
     # Create the contour plot
-    CS = plt.contourf(xi, yi, zi, detail_val, cmap=plt.cm.rainbow,
-                      vmax=zmax, vmin=zmin)
+    CS = plt.contourf(xi, yi, zi, detail, cmap=plt.cm.rainbow,vmax=zmax, vmin=zmin)
     #plt.xlim((-5,-3))
     #plt.ylim((-1,1))
     plt.xlabel("X position",size=10)
